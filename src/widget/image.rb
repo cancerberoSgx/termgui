4a0f1136e7ccdac9ca7c5d46595ce969529e5503
cancerberosgx <sebastigurin@gmail.com>
2020-02-09 08:53:43 -0300

    ElementRenderer: render_cache, dirty

--- src/widget/image.rb
+++ src/widget/image.rb
@@ -48,41 +48,41 @@ end
 
 Image = TermGui::Widget::Image
 
-require_relative '../screen'
-require_relative '../util'
-require_relative 'button'
-img = nil
-s = Screen.new(
-  children: [
-    Button.new(text: 'hello', x: 0.7, y: 0.6, action: proc { |e|
-      img ||= Image.new(
-        render_cache: true,
-        x: 2,
-        y: 1,
-        style: Style.new(
-          border: Border.new(fg: '#ee3388'),
-          padding: Bounds.new(top: 0.1, left: 0.01, right: 0.1, bottom: 0.1),
-          bg: '#e09988',
-          bold: true
-        ),
-        use_bg: false,
-        use_fg: true,
-        width: 0.9,
-        ch: '#',
-        height: 0.9,
-        src: '/Users/wyeworks/Documents/assets/whale4.png',
-        parent: s
-      )
-      s.clear
-      t0 = Time.now
-      img.render
-      text = "rendered in #{print_ms(t0)}"
-      s.text(x: random_int(1, s.width - 20), y: random_int(1, s.height - 3), text: text)
-      e.target.x = random_int(1, s.width - 20)
-      e.target.y = random_int(1, s.height - 3)
-      e.target.text = text
-      e.target.render
-    })
-  ]
-)
-s.start
+# require_relative '../screen'
+# require_relative '../util'
+# require_relative 'button'
+# img = nil
+# s = Screen.new(
+#   children: [
+#     Button.new(text: 'hello', x: 0.7, y: 0.6, action: proc { |e|
+#       img ||= Image.new(
+#         render_cache: true,
+#         x: 2,
+#         y: 1,
+#         style: Style.new(
+#           border: Border.new(fg: '#ee3388'),
+#           padding: Bounds.new(top: 0.1, left: 0.01, right: 0.1, bottom: 0.1),
+#           bg: '#e09988',
+#           bold: true
+#         ),
+#         use_bg: false,
+#         use_fg: true,
+#         width: 0.9,
+#         ch: '#',
+#         height: 0.9,
+#         src: '/Users/wyeworks/Documents/assets/whale4.png',
+#         parent: s
+#       )
+#       s.clear
+#       t0 = Time.now
+#       img.render
+#       text = "rendered in #{print_ms(t0)}"
+#       s.text(x: random_int(1, s.width - 20), y: random_int(1, s.height - 3), text: text)
+#       e.target.x = random_int(1, s.width - 20)
+#       e.target.y = random_int(1, s.height - 3)
+#       e.target.text = text
+#       e.target.render
+#     })
+#   ]
+# )
+# s.start
